39cc7651ee04065f2c47034e840cfed48f2336fb
vinnyWebDev <vinnywebdev@gmail.com>
2023-08-07 22:54:20 +0100

    reverted changes

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -2,7 +2,6 @@ class TenantsController < ApplicationController
   def index
     @tenants = Tenant.all
   end
-
   #show a specific tenant
   def show
     @tenant = Tenant.find(params[:id])
@@ -13,7 +12,7 @@ class TenantsController < ApplicationController
   end
 #creates new tenant
   def create
-    @tenant = Tenant.new(first_name: "...", last_name: "...", apartment: "...", phone: "...", email:"...")
+    @tenant = Tenant.new(tenant_params)
     #redirects to tenant if successful 
     if @tenant.save
       redirect_to @tenant
@@ -21,13 +20,32 @@ class TenantsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+#edit existing 
+  def edit
+    @tenant = Tenant.find(params[:id])
+  end
+
+  def update
+    @tenant = Tenant.find(params[:id])
+
+    if @tenant.update(tenant_params)
+      redirect_to @tenant
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  #destory tenant
+  def destroy
+    @tenant = Tenant.find(params[:id])
+    @tenant.destroy
+
+    redirect_to root_path, status: :see_other
+  end
 
   #filter parameters
   private
     def tenant_params
       params.require(:tenant).permit(:first_name, :last_name, :apartment, :phone, :email)
     end
-
 end
-
-
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,8 +1,9 @@
 class Tenant < ApplicationRecord
-    #check we have input for all fields
+    #check we have input for all fields, phone and email in particular
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :apartment, presence: true
-    validates :phone, presence: true
-    validates :email, presence: true
+    validates :phone, presence: true, length: { minimum: 10, maximum: 10 }
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
 end
