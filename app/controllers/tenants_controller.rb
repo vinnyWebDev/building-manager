5c8600e1d9d5c24b6cd2a156c0e016ef1d56ebcc
vinnyWebDev <vinnywebdev@gmail.com>
2023-08-07 16:57:00 +0100

    changes made to new tenant creation

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -7,4 +7,27 @@ class TenantsController < ApplicationController
   def show
     @tenant = Tenant.find(params[:id])
   end
+  #instantiate new tenant
+  def new
+    @tenant = Tenant.new
+  end
+#creates new tenant
+  def create
+    @tenant = Tenant.new(first_name: "...", last_name: "...", apartment: "...", phone: "...", email:"...")
+    #redirects to tenant if successful 
+    if @tenant.save
+      redirect_to @tenant
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  #filter parameters
+  private
+    def tenant_params
+      params.require(:tenant).permit(:first_name, :last_name, :apartment, :phone, :email)
+    end
+
 end
+
+
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,2 +1,8 @@
 class Tenant < ApplicationRecord
+    #check we have input for all fields
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :apartment, presence: true
+    validates :phone, presence: true
+    validates :email, presence: true
 end
