468190e3a4a472a2030da73985f9031a4f93a640
vinnyWebDev <vinnywebdev@gmail.com>
2023-08-07 22:43:50 +0100

    Revert "updated various CRUD functions"
    This reverts commit b355ad7baf60d8c96d8f3fb987c37c51b143c19f.

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,9 +1,8 @@
 class Tenant < ApplicationRecord
-    #check we have input for all fields, phone and email in particular
+    #check we have input for all fields
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :apartment, presence: true
-    validates :phone, presence: true, length: { minimum: 10, maximum: 10 }
-    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-
+    validates :phone, presence: true
+    validates :email, presence: true
 end
