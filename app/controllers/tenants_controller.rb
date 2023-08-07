b355ad7baf60d8c96d8f3fb987c37c51b143c19f
vinnyWebDev <vinnywebdev@gmail.com>
2023-08-07 21:18:12 +0100

    updated various CRUD functions

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -13,13 +13,35 @@ class TenantsController < ApplicationController
   end
 #creates new tenant
   def create
-    @tenant = Tenant.new(first_name: "...", last_name: "...", apartment: "...", phone: "...", email:"...")
+    @tenant = Tenant.new(tenant_params)
     #redirects to tenant if successful 
     if @tenant.save
       redirect_to @tenant
     else
       render :new, status: :unprocessable_entity
     end
+  end
+#edit existing 
+  def edit
+    @tenant = Tenant.find(params[:id])
+  end
+
+  def update
+    @tenant = Tenant.find(params[:id])
+
+    if @tenant.update(tenant_params)
+      redirect_to @tenant
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  #destory tenant
+  def destroy
+    @tenant = Tenant.find(params[:id])
+    @tenant.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   #filter parameters
